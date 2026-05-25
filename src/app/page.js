188636f0ea4f3d86d48d929bcb9e8aff0a35c4dc
tanzid-48 <mdtanzid.525@gmail.com>
2026-05-25 17:37:27 +0600

    feat: add Pricing section

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,15 @@
 
 import HeroSection from "@/components/HeroSection";
+import Pricing from "@/components/Pricing";
 
 
 export default function Home() {
   return (
     <>
       <div>
-       
+        
         <HeroSection />
+        <Pricing />
       </div>
     </>
   );
