3100fa24c0c11e1506d1664235ff4176fd5557bd
tanzid-48 <mdtanzid.525@gmail.com>
2026-06-03 13:37:26 +0600

    Create recruiter job posting form UI with validation and dynamic inputs

---
+++ src/app/dashboard/recruiter/jobs/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RecruiterJobsPage = () => {
+    return (
+        <div>
+            <h2>Recruiter/company manage All Jobs</h2>
+        </div>
+    );
+};
+
+export default RecruiterJobsPage;
